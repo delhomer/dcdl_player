c356166793f947b550a4d86825a9e81a2e42715e
Raphaël Delhome <raphael.delhome@oslandia.com>
2017-07-05 21:55:32 +0200

    Improve the game design, put results and scores into dedicated lists

--- main.py
+++ main.py
@@ -131,13 +131,22 @@ def compute_figure_score(target, result):
 def timeout():
     print("\nTemps écoulé!\n")
 
-def run(nb_set, repartition):
+def run(nb_set, repartition, letter_chrono=30, figure_chrono=40):
     """Main program: play nb_set times a set of x figure draw(s) and y letter
     draw(s), repartition giving how y is being larger than x 
     
-    """
+    """ 
+    random.seed(123)
+    results = []
+    scores = []
     for i in range(1, 1+nb_set):
         for i in range(1, 1+repartition):
             nb_voyels = random.randint(2,8)
-            print(''.join(letter_picking(nb_voyels)))
-        print(figure_picking())
+            cur_letter_game = letter_game(nb_voyels, 15)
+            results.append(cur_letter_game[0])
+            scores.append(cur_letter_game[1])
+        cur_figure_game = figure_game(15)
+        results.append(cur_figure_game[0])
+        scores.append(cur_figure_game[1])
+    print("Votre feuille de résultats: {0}".format(results))
+    print("Vous avez marqué un total de {0} points durant cette partie!".format(sum(scores)))
